31b2f74bb92cdd0f9ec5ec34ce24bf83fe294429
Arselene <arselene.dev@gmail.com>
2025-08-26 16:55:18 +0100

    - FIX: fixed an issue that prevents the RewardsScreen from showing up at the end of a lesson.

--- lib/features/lessons/controllers/lesson_controller.dart
+++ lib/features/lessons/controllers/lesson_controller.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:le_petit_davinci/features/lessons/models/activity_model.dart';
 import 'package:le_petit_davinci/features/lessons/views/reward.dart';
-import 'package:le_petit_davinci/services/progress_service.dart';
 
 class LessonsController extends GetxController {
-  // --- Dependencies are now explicit and required ---
   final Lesson lessonData;
   final int levelNumber;
   final String language;
@@ -18,24 +16,33 @@ class LessonsController extends GetxController {
   });
 
   late final PageController pageController;
-
-  /// The current page index in the PageView.
-  /// Page 0 is the intro, pages 1+ are activities.
   final RxInt currentPage = 0.obs;
 
-  /// A subscription to listen to the current activity's completion status.
+  //? A subscription to listen to the current activity's completion status.
   StreamSubscription? _completionSubscription;
 
   @override
   void onInit() {
     super.onInit();
     pageController = PageController();
-
-    // This worker will re-run our listener setup every time the page changes.
-    ever(currentPage, (_) => _setupCompletionListener());
-
-    // Set up the listener for the initial state (the first activity).
+    pageController.addListener(_onPageChanged);
     _setupCompletionListener();
+  }
+
+  @override
+  void onClose() {
+    pageController.dispose();
+    _completionSubscription?.cancel();
+    super.onClose();
+  }
+
+  void _onPageChanged() {
+    final newPage = pageController.page?.round() ?? 0;
+    if (newPage != currentPage.value) {
+      currentPage.value = newPage;
+      //? When the page changes, set up the listener for the new activity.
+      _setupCompletionListener();
+    }
   }
 
   void _setupCompletionListener() {
@@ -46,7 +53,7 @@ class LessonsController extends GetxController {
       final activity = lessonData.activities[activityIndex];
       _completionSubscription = activity.isCompleted.listen((isDone) {
         if (isDone) {
-          activity.dispose(); // Clean up the completed activity's resources.
+          activity.dispose(); //? Clean up the completed activity's resources.
           WidgetsBinding.instance.addPostFrameCallback((_) => nextStage());
         }
       });
@@ -57,41 +64,33 @@ class LessonsController extends GetxController {
   void nextStage() {
     if (!pageController.hasClients) return;
 
-    // We check against `lessonData.activities.length` because there's one extra page for the intro.
-    if (currentPage.value < lessonData.activities.length) {
-      currentPage.value++;
+    if (currentPage.value < lessonData.activities.length - 1) {
+      //? We just tell the PageView to move. The listener will handle the state.
       pageController.nextPage(
         duration: const Duration(milliseconds: 350),
         curve: Curves.easeIn,
       );
     } else {
-      // Lesson finished!
+      //? If we are on the last page, complete the lesson.
       _completeLessonAndUnlock();
     }
   }
 
-  @override
-  void onClose() {
-    _completionSubscription?.cancel();
-    pageController.dispose();
-    super.onClose();
-  }
-
   /// Persists the lesson completion and unlocks the next level.
   Future<void> _completeLessonAndUnlock() async {
-    try {
-      if (Get.isRegistered<ProgressService>()) {
-        // Mark as completed by awarding 3 stars.
-        await ProgressService.instance.setStars(language, levelNumber, 3);
-        // Unlock the next level.
-        await ProgressService.instance.unlockNextIfNeeded(
-          language,
-          levelNumber,
-        );
-      }
-    } catch (e) {
-      debugPrint('Lesson completion persistence failed: $e');
-    }
+    // try {
+    //   if (Get.isRegistered<ProgressService>()) {
+    //     // Mark as completed by awarding 3 stars.
+    //     await ProgressService.instance.setStars(language, levelNumber, 3);
+    //     // Unlock the next level.
+    //     await ProgressService.instance.unlockNextIfNeeded(
+    //       language,
+    //       levelNumber,
+    //     );
+    //   }
+    // } catch (e) {
+    //   debugPrint('Lesson completion persistence failed: $e');
+    // }
     // Navigate to the reward screen.
     Get.off(() => const RewardScreen());
   }
